3b334ed375f334ff651ccd3f8cf8d2e3d89f8ccb
50chin <50chin50@gmail.com>
2024-03-27 15:48:36 +0300

    finish

--- src/script/app.js
+++ src/script/app.js
@@ -1,17 +1,17 @@
-import { data } from "./data.js";
-const cardsNode = document.querySelector(".cards");
-const inputNode = document.querySelector("input");
-const selectNode = document.querySelector("select");
-const imgNode = document.querySelector(".card__img");
+import { data } from './data.js';
+const cardsNode = document.querySelector('.cards');
+const inputNode = document.querySelector('input');
+const selectNode = document.querySelector('select');
+const imgNode = document.querySelector('.card__img');
 
-selectNode.addEventListener("change", selectChoice);
+selectNode.addEventListener('change', selectChoice);
 
+let choice;
 function selectChoice(evt) {
   let value = evt.target.value;
-  let choice;
-  if (value === "Not Found") {
-    choice = data.filter((el) => el.house.toLowerCase() === "");
-  } else if (value === "All") {
+  if (value === 'Not Found') {
+    choice = data.filter((el) => el.house.toLowerCase() === '');
+  } else if (value === 'All') {
     choice = data;
   } else {
     choice = data.filter(
@@ -21,12 +21,12 @@ function selectChoice(evt) {
   renderCards(choice);
 }
 
-inputNode.addEventListener("input", inputHandler);
+inputNode.addEventListener('input', inputHandler);
 
 function inputHandler(evt) {
   let value = evt.target.value;
 
-  const finder = data.filter(
+  const finder = choice.filter(
     (el) =>
       el.name.toLowerCase().includes(value.toLowerCase()) ||
       el.actor.toLowerCase().includes(value.toLowerCase())
@@ -35,10 +35,10 @@ function inputHandler(evt) {
 }
 
 function renderCards(data) {
-  cardsNode.innerHTML = "";
+  cardsNode.innerHTML = '';
   data.forEach((el) => {
-    const card = document.createElement("div");
-    card.className = "card";
+    const card = document.createElement('div');
+    card.className = 'card';
     card.innerHTML = `
     <img class="card__img" ${
       el.image == false ? 'src="./src/img/person.jpeg"' : `src="${el.image}"`
@@ -46,16 +46,16 @@ function renderCards(data) {
     <div class="card__desc">
     <h2 class="card__name">${el.name}</h2>
     <p class="card__bio">Actor: ${
-      el.actor == "" ? "not found " : `${el.actor}`
+      el.actor == '' ? 'not found ' : `${el.actor}`
     }</p>
     <p class="card__bio">Gender: ${el.gender}</p>
     <p class="card__bio">House: ${
-      el.house == "" ? "not found" : `${el.house}`
+      el.house == '' ? 'not found' : `${el.house}`
     }</p>
     <p class="card__bio">Wand core: ${
-      el.wand.core == "" ? "not found" : `${el.wand.core}`
+      el.wand.core == '' ? 'not found' : `${el.wand.core}`
     }</p>
-    <p class="card__bio">Alive: ${el.alive ? "yes" : "no"}</p>
+    <p class="card__bio">Alive: ${el.alive ? 'yes' : 'no'}</p>
     </div>
     `;
     cardsNode.append(card);
